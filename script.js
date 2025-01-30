3c5b78c0c6640e17505e7d4b30c59706bf1ed38c
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2025-01-30 11:01:48 -0500

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function () {
+  const inputValue = Number(document.getElementById("ip").value);
+  const outputDiv = document.getElementById("output");
+
+  function delay(value, time) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(value), time);
+    });
+  }
+
+  delay(inputValue, 2000)
+    .then((num) => {
+      outputDiv.textContent = `Result: ${num}`;
+      return delay(num * 2, 1000);
+    })
+    .then((num) => {
+      outputDiv.textContent = `Result: ${num}`;
+      return delay(num - 3, 1000);
+    })
+    .then((num) => {
+      outputDiv.textContent = `Result: ${num}`;
+      return delay(num / 2, 1000);
+    })
+    .then((num) => {
+      outputDiv.textContent = `Result: ${num}`;
+      return delay(num + 10, 1000);
+    })
+    .then((num) => {
+      outputDiv.textContent = `Final Result: ${num}`;
+    })
+    .catch((error) => {
+      outputDiv.textContent = `Error: ${error}`;
+    });
+});
